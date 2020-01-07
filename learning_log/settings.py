1741c802b0a3921a7059a66fc6d5f87a4f967267
Tommy <zsd26922000@outlook.com>
2020-01-07 22:24:04 +0800

    0011.2

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -22,6 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 try:
     with open('learning_log/secret_key.txt') as f:
         SECRET_KEY = f.read().strip()
+except Exception as e:
+    print(e)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
